8551d9836c7a4ac40877aa63185233fc843d23cf
KhaoulaElmajni <50221822+KhaoulaElmajni@users.noreply.github.com>
2020-03-15 15:40:35 +0100

    ESTSB SITE

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
-<footer>
-	<p>
-		© 2020 khaoula imane - Tous Droits réservés
-	</p>
+<footer class="w3-container w3-card-4 w3-center w3-margin-top" style="color: blue">
+  <p><b><u>Find us on social media.</u></b></p>
+  <i class="fa fa-facebook-official w3-hover-opacity"><a href="https://web.facebook.com/groups/123496191713259/?epa=SEARCH_BOX">ESTSB</a></i>
+  <br>
+  <i class="fa fa-instagram w3-hover-opacity"><a href="https://www.instagram.com/estsidibennour/?hl=fr">ESTSB</a></i>
+  <br>
+   <i class="fa font-awesome w3-hover-opacity"><a href="http://www.estsb.ucd.ac.ma/">ESTSB</a></i>
+  <br>
+    
+  <p>© REALISER PAR FRIENDS KHAOULA & IMANE & OUMAIMA </p>
 </footer>
-
-</body>
-</html>
